0d41ebfce880cfa124343cb86b02e43c37b24f86
NADESHIKON <allenzhang040817@gmail.com>
2023-01-15 15:35:48 -0800

    Forward queries + add user-agent header

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use std::fmt::format;
+use std::str::FromStr;
 use std::time::Duration;
 use actix_cors::Cors;
 use actix_web::{get, web, Result, options, HttpRequest, HttpResponse};
 use actix_web::http::header;
 use actix_web::web::resource;
-use reqwest::{ClientBuilder, Url};
+use reqwest::{ClientBuilder, Url, header as request_header};
 use urlencoding::{decode, encode};
 use url::Url as RustUrl;
 use serde::Deserialize;
 
 pub struct CORS;
+
+static USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36";
 
 #[get("/")]
 async fn index() -> &'static str {
@@ -58,11 +61,17 @@ async fn proxy(req: HttpRequest) -> HttpResponse {
     let raw_meta = req.match_info().query("meta");
     let mut supplied_meta = decode(raw_meta).expect("UTF-8");
     let file = req.match_info().get("file").unwrap();
+    let queries = req.query_string();
+
+    let mut headers = header::HeaderMap::new();
+    headers.insert(request_header::HeaderName::from_str("user-agent").unwrap(), request_header::HeaderValue::from_str(USER_AGENT).unwrap());
 
     let timeout = Duration::new(5, 0);
     let client = ClientBuilder::new().timeout(timeout).build().unwrap();
 
-    let response = client.get(supplied_meta.to_string() + "/" + file).send().await.unwrap();
+    let response = client
+        .get(supplied_meta.to_string() + "/" + file + "?" + queries)
+        .send().await.unwrap();
 
     return HttpResponse::Ok()
         .insert_header(("content-type", response.headers().get("content-type").unwrap().to_str().unwrap()))
@@ -80,6 +89,7 @@ async fn main() -> std::io::Result<()> {
         .service(redirect)
     )
         .bind(("0.0.0.0", 80))?
+        .bind(("127.0.0.1", 8000))?
         .run()
         .await
 }
